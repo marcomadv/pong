05663a84a02aedeceed9fdc03b3dedb12fb9a8d1
marcomadv <imarcoma@gmail.com>
2023-03-31 21:54:20 +0200

    movimiento palas y bolas

--- figura_class.py
+++ figura_class.py
@@ -13,6 +13,15 @@ class Raqueta:
     def dibujarRaqueta(self, pantalla):
         pg.draw.rect(pantalla, self.color,(self.pos_x, self.pos_y, self.w, self.h))
 
+    def moverRaqueta(self, tecla_arriba, tecla_abajo, y_max = 600, y_min = 0):
+        estado_teclado = pg.key.get_pressed()
+
+        if estado_teclado[tecla_arriba] == True and self.pos_y > y_min:
+            self.pos_y -= 3
+
+    
+        if estado_teclado[tecla_abajo] == True and self.pos_y < y_max - (self.h):
+            self.pos_y += 3
 
 
 class Pelota:
@@ -23,8 +32,27 @@ class Pelota:
         self.radio = radio
         self.vx = vx
         self.vy = vy
+
+        self.contadorDerecho = 0
+        self.contadorIzquierdo = 0
+
     def dibujarPelota(self, pantalla):
         pg.draw.circle(pantalla, self.color, (self.pos_x, self.pos_y,), self.radio)
 
+    def moverPelota(self, x_max = 800, y_max = 600):
+        self.pos_x += self.vx
+        self.pos_y += self.vy
 
+     
+        if self.pos_y >= y_max-self.radio or self.pos_y < 0 + self.radio:
+            self.vy *= -1
         
+
+        if self.pos_x >= x_max + self.radio: #limite derecha
+            self.vx *= -1
+            self.contadorIzquierdo += 1
+
+        if self.pos_x < 0 - self.radio * 5 : #limite izquierdo
+            self.vx *= -1
+            self.contadorDerecho += 1
+
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from figura_class import Pelota, Raqueta
 pg.init()
 
 pantalla_principal = pg.display.set_mode((800, 600))
-pg.display.set_caption("pong")
+pg.display.set_caption("Pong")
 #definir tasa de refresco de nuestro bucle de fps
 tasa_refresco = pg.time.Clock()
 
@@ -16,12 +16,19 @@ game_over = False
 
 while not game_over:
     #obtener tasa de refreso en milisengundos
-    valor_tasa = tasa_refresco.tick(60) #variables para controlar la velocidad entre fotogramas (60 fps)
+    valor_tasa = tasa_refresco.tick(300) #variables para controlar la velocidad entre fotogramas (60 fps)
     #print(valor_tasa)
 
     for evento in pg.event.get():
         if evento.type == pg.QUIT:
             game_over = True
+
+    raqueta1.moverRaqueta(pg.K_w, pg.K_s)   
+    raqueta2.moverRaqueta(pg.K_UP, pg.K_DOWN)
+    pelota.moverPelota()
+
+    print("Punto player der:", pelota.contadorDerecho)
+    print("Punto player izq:", pelota.contadorIzquierdo )
 
     pantalla_principal.fill((0, 0, 0)) #color negro rgb pantalla principal
     pg.draw.line(pantalla_principal, (255, 255, 255),(400, 0), (400, 600), width = 6)
@@ -29,6 +36,7 @@ while not game_over:
     pelota.dibujarPelota(pantalla_principal)
     raqueta1.dibujarRaqueta(pantalla_principal)
     raqueta2.dibujarRaqueta(pantalla_principal)
+
 
     pg.display.flip()
 
